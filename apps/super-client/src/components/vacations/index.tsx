1835e69e4afcaba8e5d9d8897f23b25cbbe9d0a7
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-07-24 01:18:08 +0300

    a

--- apps/super-client/src/components/vacations/index.tsx
+++ apps/super-client/src/components/vacations/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef, MutableRefObject, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,60 +10,37 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
-import { setTimeout } from 'timers/promises';
 
 
-const m = "You are clicking too much!"
+
+
 export default function Vacations() {
     const initialVacations: Array<string> = ["Ibiza", "Sayshel"]
     const [vacations, setVacations] = useState(initialVacations)
     const [currentVacation, setCurrentVacation] = useState("Hawai")
     const [counter, setCounter] = useState(0)
-    const [showMessage, setShowMessage] = useState(false)
-    const refVacations = useRef(0)
-    console.log("render please!")
+
+
     const addVacationHandler = useCallback(() => {
         setVacations([...vacations, currentVacation])
     }, [currentVacation, vacations])
 
-
-    // const setCounterHandler = useCallback(() => {
-    //     refVacations.current++;
-    //     console.log("Counter clicked " + refVacations.current)
-    //     setCounter(counter + 1)
-    // }, [counter])
-
-    const setCounterHandler = () => {
-        refVacations.current++;
-        console.log("Counter clicked ", refVacations.current)
-        if (refVacations.current >= 10) {
-            refVacations.current = 0;
-            setShowMessage(true);
-            window.setTimeout(() => {
-                setShowMessage(false)
-            }, 2000);
-        }
-        // setCounter(counter + 1)
-    }
+    const setCounterHandler = useCallback(() => {
+        setCounter(counter + 1)
+    }, [counter])
 
     return (
         <div style={{ padding: "10px", width: "50%", margin: "auto auto", border: "1px solid black", borderRadius: 10 }}>
-            {JSON.stringify(refVacations.current)}
-            <AccessingElement />
             <FormGroup row>
                 <TextField value={currentVacation} onChange={(e: any) => {
                     setCurrentVacation(e?.target?.value)
                 }} id="standard-basic" label="Insert Vacation" />
-                <Button onClick={() => {
-                    // addVacationHandlerRef()
-                    addVacationHandler()
-                }} variant="contained" color="primary" disableElevation>
+                <Button onClick={addVacationHandler} variant="contained" color="primary" disableElevation>
                     Add
                 </Button>
                 <Button onClick={setCounterHandler} variant="contained" color="primary" disableElevation>
                     increase
                 </Button>
-                {showMessage && m}
             </FormGroup>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
@@ -82,8 +59,8 @@ export default function Vacations() {
 
     function VacationsList() {
         return <List >
-            {vacations.map((v: string, index: number) => {
-                return <ListItem key={v + index}>
+            {vacations.map((v: string) => {
+                return <ListItem>
                     <ListItemText
                         primary={v}
                     />
@@ -91,29 +68,4 @@ export default function Vacations() {
             })}
         </List>
     }
-}
-
-function AccessingElement() {
-    const elementRef = useRef<HTMLInputElement>(null);
-    const [list, setList] = useState<Array<string | undefined>>([])
-
-    useEffect(() => {
-        if (elementRef && elementRef.current) {
-            elementRef.current.value = "defaultValue"
-        }
-    }, [])
-
-
-    return (
-        <div>
-            <h1> Input ref element {elementRef?.current?.value}</h1>
-            <input ref={elementRef} />
-            <button onClick={() => {
-                setList([...list, elementRef?.current?.value])
-            }}> set list </button>
-            <div>
-                {list.map(i => <h2>{i}</h2>)}
-            </div>
-        </div>
-    );
 }
