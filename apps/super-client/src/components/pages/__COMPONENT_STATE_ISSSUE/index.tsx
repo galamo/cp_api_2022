f05f82d312475a2518d304727ac101b73a58b13f
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-08-04 13:26:30 +0300

    issue

---
+++ apps/super-client/src/components/pages/__COMPONENT_STATE_ISSSUE/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+
+export function ParentProblem() {
+    const [n, setN] = useState(1)
+    return <div>
+        <button className="btn btn-primary"
+            onClick={() => { setN(n + 1) }}>
+            Increase N GGGParent! </button>
+        <ChildA n={n} />
+        <ChildB />
+    </div>
+}
+
+function ChildA(props: { n: number }) {
+    return <div>
+        <GrandChildOfA n={props.n} />
+    </div>
+}
+
+function GrandChildOfA(props: { n: number }) {
+    return <div><Grand2ChildOfA n={props.n} /></div>
+}
+
+function Grand2ChildOfA(props: { n: number }) {
+    return <h1>This number from GGGParent (Dec): {props.n} </h1>
+}
+
+function ChildB() {
+    return <div> </div>
+}
